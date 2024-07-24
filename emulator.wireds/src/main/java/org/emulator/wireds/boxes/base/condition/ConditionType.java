c0f7aeb2ed46980711d76f7347bd79709e121f61
IDjinn <42049518+IDjinn@users.noreply.github.com>
2024-07-24 11:38:37 -0300

    init

--- emulator.wireds/src/main/java/org/emulator/wireds/boxes/base/condition/ConditionType.java
+++
@@ -1,6 +0,0 @@
-package org.emulator.wireds.boxes.base.condition;
-
-public enum ConditionType {
-    Identity,
-    Negation
-}
--- emulator.wireds/src/main/java/org/emulator/wireds/boxes/util/codes/WiredInterfaceCode.java
+++
@@ -1,5 +0,0 @@
-package org.emulator.wireds.boxes.util.codes;
-
-public interface WiredInterfaceCode {
-    int getCode();
-}
